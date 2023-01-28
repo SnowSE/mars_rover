b0fa5001d6dbe5df9c993b0dfd1748f0aa61a9ec
Caleb Wightman <agentcboy@gmail.com>
2023-01-27 20:10:27 -0700

    test for move api

---
+++ src/Mars.Web.Tests/MoveRoverTests.cs
@@ -0,0 +1,40 @@
+﻿using Mars.Web.Types;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars.Web.Tests
+{
+    internal class MoveRoverTests
+    {
+        private WebApplicationFactory<Program> _factory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+
+                    });
+                });
+        }
+
+        [Test]
+        public async Task CanMoveRover()
+        {
+            var client = _factory.CreateClient();
+            var joinResponse = await client.GetFromJsonAsync<JoinResponse>("/game/join?name=Caleb");
+
+            var moveResponse = await client.GetAsync($"/move?token={joinResponse.Token}&direction=Forward");
+
+            moveResponse.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
