645b1aa1e355f26600dccf4ab5d52fa4b59bc311
Jonathan Allen <jallen@snow.edu>
2023-01-30 12:45:00 -0700

    fix state changes and event handling for making a new game

--- src/Mars.Web/GameManager.cs
+++ src/Mars.Web/GameManager.cs
@@ -4,14 +4,34 @@ public class GameManager
 {
 	public GameManager()
 	{
-		Game = new Game();
+		StartNewGame(new GameStartOptions()
+		{
+			Height = 100,
+			Width = 100
+		});
 	}
 
 	public Game Game { get; private set; }
+	public event EventHandler? GameStateChanged;
 
 	public void StartNewGame(GameStartOptions startOptions)
 	{
+		//unsubscribe from old event
+		if (Game != null)
+		{
+			Game.GameStateChanged -= Game_GameStateChanged;
+		}
+
 		Game = new Game(startOptions.Width, startOptions.Height);
+		GameStateChanged?.Invoke(this, EventArgs.Empty);
+
+		//subscribe to new event
+		Game.GameStateChanged += Game_GameStateChanged;
+	}
+
+	private void Game_GameStateChanged(object? sender, EventArgs e)
+	{
+		GameStateChanged?.Invoke(this, e);
 	}
 }
 
