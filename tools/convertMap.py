e01432ff9ce65490f2a16eac5ffe8043c32c0a31
Adam Teichert <adam.teichert@snow.edu>
2023-01-31 09:14:34 -0700

    added failing test for unsigned types

--- tools/convertMap.py
+++ tools/convertMap.py
@@ -36,6 +36,26 @@ def test_intensities_to_damages():
     observed = intensities_to_damages(map)
     assert (expected == observed).all()
 
+def test_intensities_to_damages_unsigned():
+    map = np.array([
+        [1, 2, 5, 3],
+        [1, 3, 6, 4],
+        [0, 3, 6, 5],
+        [1, 5, 4, 7]
+    ], dtype=np.uint)
+    v1 = abs(3-2) + abs(3-6) + abs(3-3) + abs(3-1)
+    v2 = abs(6-5) + abs(6-4) + abs(6-6) + abs(6-3)
+    v3 = abs(3-3) + abs(3-6) + abs(3-5) + abs(3-0)
+    v4 = abs(6-6) + abs(6-5) + abs(6-4) + abs(6-3)
+    expected = np.array([
+        [0, 0, 0, 0],
+        [0, v1, v2, 0],
+        [0, v3, v4, 0],
+        [0, 0, 0, 0],
+    ])
+    observed = intensities_to_damages(map)
+    assert (expected == observed).all()
+
 
 def main():
     parser = ArgumentParser('convertMap', description="Converts image file to json damage map (list of lists) which is printed to the console")
