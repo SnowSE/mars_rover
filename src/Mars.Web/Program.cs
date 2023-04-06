071e0aea457861d893cc4e363534472830d36946
andrewbengordon <andrewbengordon@gmail.com>
2023-04-06 12:46:02 -0600

    testing appbase

--- src/Mars.Web/Program.cs
+++ src/Mars.Web/Program.cs
@@ -76,6 +76,8 @@ builder.Services.AddRateLimiter(options =>
 
 var app = builder.Build();
 
+app.UsePathBase(builder.Configuration["PathBase"] ?? string.Empty);
+
 if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
