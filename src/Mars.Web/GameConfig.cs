dd951cc5fcbea6fa623cdb8d90c7d83b811d227a
Jonathan Allen <jallen@snow.edu>
2023-04-25 09:09:32 -0600

    add a bit of validation on game config

--- src/Mars.Web/GameConfig.cs
+++ src/Mars.Web/GameConfig.cs
@@ -1,8 +1,12 @@
-﻿namespace Mars.Web;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mars.Web;
 
 public class GameConfig
 {
+	[Required(AllowEmptyStrings = false)]
 	public string Password { get; set; } = "password";
+	[Range(10, 100_000)]
 	public int ApiLimitPerSecond { get; set; } = 50;
 	public int MaxMaps { get; set; } = 2;
 	public int DefaultMap { get; set; } = 0;
--- src/Mars.Web/Program.cs
+++ src/Mars.Web/Program.cs
@@ -14,45 +14,47 @@ using System.Threading.RateLimiting;
 var builder = WebApplication.CreateBuilder(args);
 
 using var traceProvider = Sdk.CreateTracerProviderBuilder()
-    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Mars.Web"))
-    .AddSource(GameActivitySource.Instance.Name)
-    .AddJaegerExporter(o =>
-    {
-        o.Protocol = OpenTelemetry.Exporter.JaegerExportProtocol.HttpBinaryThrift;
-        o.Endpoint = new Uri("http://jaeger:14268/api/traces");
-    })
-    .AddHttpClientInstrumentation()
-    .AddAspNetCoreInstrumentation()
-    .Build();
+	.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Mars.Web"))
+	.AddSource(GameActivitySource.Instance.Name)
+	.AddJaegerExporter(o =>
+	{
+		o.Protocol = OpenTelemetry.Exporter.JaegerExportProtocol.HttpBinaryThrift;
+		o.Endpoint = new Uri("http://jaeger:14268/api/traces");
+	})
+	.AddHttpClientInstrumentation()
+	.AddAspNetCoreInstrumentation()
+	.Build();
 
-builder.Services.AddOptions<GameConfig>().BindConfiguration(nameof(GameConfig));
+builder.Services.AddOptions<GameConfig>()
+	.BindConfiguration(nameof(GameConfig))
+	.ValidateDataAnnotations()
+	.ValidateOnStart();
 builder.Services.AddApplicationInsightsTelemetry();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+	options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Host.UseSerilog((context, loggerConfig) =>
 {
-    loggerConfig.WriteTo.Console()
-    .Enrich.WithExceptionDetails()
-    .WriteTo.Seq(builder.Configuration["SeqServer"] ?? throw new ApplicationException("Unable to locate key SeqServer in configuration"));
+	loggerConfig.WriteTo.Console()
+	.Enrich.WithExceptionDetails()
+	.WriteTo.Seq(builder.Configuration["SeqServer"] ?? throw new ApplicationException("Unable to locate key SeqServer in configuration"));
 });
 
 builder.Services.AddProblemDetails(opts =>
 {
-    opts.IncludeExceptionDetails = (ctx, ex) => false;
-}
-);
+	opts.IncludeExceptionDetails = (ctx, ex) => false;
+});
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mars Rover", Version = "v1" });
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mars Rover", Version = "v1" });
+	var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
 builder.Services.AddSingleton<MultiGameHoster>();
@@ -62,31 +64,31 @@ builder.Services.AddHostedService<CleanupGameService>();
 
 builder.Services.AddRateLimiter(options =>
 {
-    options.RejectionStatusCode = 429;
-    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
-    {
-        var token = httpContext.Request.Query["token"].FirstOrDefault();
-        var ipAddress = httpContext.Request.Headers.Host.ToString();
-        var apiLimit = httpContext.RequestServices.GetRequiredService<GameConfig>().ApiLimitPerSecond;
+	options.RejectionStatusCode = 429;
+	options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+	{
+		var token = httpContext.Request.Query["token"].FirstOrDefault();
+		var ipAddress = httpContext.Request.Headers.Host.ToString();
+		var apiLimit = httpContext.RequestServices.GetRequiredService<GameConfig>().ApiLimitPerSecond;
 
-        return RateLimitPartition.GetFixedWindowLimiter(
-            partitionKey: token ?? ipAddress,
-            factory: partition => new FixedWindowRateLimiterOptions
-            {
-                AutoReplenishment = true,
-                PermitLimit = apiLimit,
-                QueueLimit = 0,
-                Window = TimeSpan.FromSeconds(1)
-            });
-    });
+		return RateLimitPartition.GetFixedWindowLimiter(
+			partitionKey: token ?? ipAddress,
+			factory: partition => new FixedWindowRateLimiterOptions
+			{
+				AutoReplenishment = true,
+				PermitLimit = apiLimit,
+				QueueLimit = 0,
+				Window = TimeSpan.FromSeconds(1)
+			});
+	});
 });
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseStaticFiles();
@@ -96,7 +98,7 @@ app.UseRouting();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mars Rover v1");
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mars Rover v1");
 });
 
 app.MapBlazorHub()
