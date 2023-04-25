f3e2a4d149b8c855bc0338c43dfd2e1f7c58c8c9
Jonathan Allen <jallen@snow.edu>
2023-04-25 09:26:50 -0600

    start including some xml comments on more public types

--- src/Mars.Web/CleanupGameService.cs
+++ src/Mars.Web/CleanupGameService.cs
@@ -2,35 +2,35 @@ namespace Mars.Web;
 
 public class CleanupGameService : BackgroundService
 {
-    private readonly MultiGameHoster multiGameHoster;
-    private readonly ILogger<CleanupGameService> logger;
-    public readonly int CleanupFrequencyMinutes;
+	private readonly MultiGameHoster multiGameHoster;
+	private readonly ILogger<CleanupGameService> logger;
+	private readonly int cleanupFrequencyMinutes;
 
-    public CleanupGameService(MultiGameHoster multiGameHoster, ILogger<CleanupGameService> logger, GameConfig gameConfig)
-    {
-        this.multiGameHoster = multiGameHoster;
-        this.logger = logger;
-        CleanupFrequencyMinutes = gameConfig.CleanupFrequencyMinutes;
-    }
+	public CleanupGameService(MultiGameHoster multiGameHoster, ILogger<CleanupGameService> logger, GameConfig gameConfig)
+	{
+		this.multiGameHoster = multiGameHoster;
+		this.logger = logger;
+		cleanupFrequencyMinutes = gameConfig.CleanupFrequencyMinutes;
+	}
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            await Task.Delay(TimeSpan.FromMinutes(CleanupFrequencyMinutes), stoppingToken);
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		while (!stoppingToken.IsCancellationRequested)
+		{
+			await Task.Delay(TimeSpan.FromMinutes(cleanupFrequencyMinutes), stoppingToken);
 
-            logger.LogInformation("Looking for any old games to clean up:");
-            var oldGames = multiGameHoster.Games.Where(g => g.Value.CreatedOn < DateTime.Now.AddMinutes(CleanupFrequencyMinutes * -1)).ToArray();
-            foreach (var oldGame in oldGames)
-            {
-                logger.LogInformation("Cleaning up game {gameId}", oldGame.Key);
-                multiGameHoster.Games.Remove(oldGame.Key, out _);
-            }
+			logger.LogInformation("Looking for any old games to clean up:");
+			var oldGames = multiGameHoster.Games.Where(g => g.Value.CreatedOn < DateTime.Now.AddMinutes(cleanupFrequencyMinutes * -1)).ToArray();
+			foreach (var oldGame in oldGames)
+			{
+				logger.LogInformation("Cleaning up game {gameId}", oldGame.Key);
+				multiGameHoster.Games.Remove(oldGame.Key, out _);
+			}
 
-            if (oldGames.Any())
-            {
-                multiGameHoster.RaiseOldGamesPurged();
-            }
-        }
-    }
+			if (oldGames.Any())
+			{
+				multiGameHoster.RaiseOldGamesPurged();
+			}
+		}
+	}
 }
--- src/Mars.Web/Controllers/AdminController.cs
+++ src/Mars.Web/Controllers/AdminController.cs
@@ -1,53 +1,74 @@
 namespace Mars.Web.Controllers;
 
+/// <summary>
+/// Administrator options for the game
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class AdminController : ControllerBase
 {
-    private readonly GameConfig gameConfig;
-    private readonly ILogger<AdminController> logger;
-    private readonly MultiGameHoster gameHoster;
+	private readonly GameConfig gameConfig;
+	private readonly ILogger<AdminController> logger;
+	private readonly MultiGameHoster gameHoster;
 
-    public AdminController(GameConfig gameConfig, ILogger<AdminController> logger, MultiGameHoster gameHoster)
-    {
-        this.gameConfig = gameConfig;
-        this.logger = logger;
-        this.gameHoster = gameHoster;
-    }
+	public AdminController(GameConfig gameConfig, ILogger<AdminController> logger, MultiGameHoster gameHoster)
+	{
+		this.gameConfig = gameConfig;
+		this.logger = logger;
+		this.gameHoster = gameHoster;
+	}
 
-    [HttpPost("[action]")]
-    [ProducesResponseType(typeof(string), 200)]
-    [ProducesResponseType(typeof(ProblemDetails), 400)]
-    public IActionResult StartGame(StartGameRequest request)
-    {
-        if (request.Password != gameConfig.Password)
-            return Problem("Invalid password", statusCode: 400, title: "Cannot start game with invalid password.");
+	/// <summary>
+	/// Allow an admin to begin game play without needing to interact with the game UI
+	/// </summary>
+	/// <param name="request"></param>
+	/// <returns></returns>
+	[HttpPost("[action]")]
+	[ProducesResponseType(typeof(string), 200)]
+	[ProducesResponseType(typeof(ProblemDetails), 400)]
+	public IActionResult StartGame(StartGameRequest request)
+	{
+		if (request.Password != gameConfig.Password)
+			return Problem("Invalid password", statusCode: 400, title: "Cannot start game with invalid password.");
 
-        if (gameHoster.Games.TryGetValue(request.GameID, out var gameManager))
-        {
-            try
-            {
-                var gamePlayOptions = new GamePlayOptions
-                {
-                    RechargePointsPerSecond = request.RechargePointsPerSecond,
-                };
-                logger.LogInformation("Starting game play via admin api");
-                gameManager.Game.PlayGame(gamePlayOptions);
-                return Ok("Game started OK");
-            }
-            catch (Exception ex)
-            {
-                return Problem(ex.Message, statusCode: 400, title: "Error starting game");
-            }
-        }
+		if (gameHoster.Games.TryGetValue(request.GameID, out var gameManager))
+		{
+			try
+			{
+				var gamePlayOptions = new GamePlayOptions
+				{
+					RechargePointsPerSecond = request.RechargePointsPerSecond,
+				};
+				logger.LogInformation("Starting game play via admin api");
 
-        return Problem("Invalid GameID", statusCode: 400, title: "Invalid Game ID");
-    }
+				gameManager.Game.PlayGame(gamePlayOptions);
+				return Ok("Game started OK");
+			}
+			catch (Exception ex)
+			{
+				return Problem(ex.Message, statusCode: 400, title: "Error starting game");
+			}
+		}
+
+		return Problem("Invalid GameID", statusCode: 400, title: "Invalid Game ID");
+	}
 }
 
+/// <summary>
+/// Configuration options that can change per game instance
+/// </summary>
 public class StartGameRequest
 {
-    public int RechargePointsPerSecond { get; set; }
-    public string? Password { get; set; }
-    public string GameID { get; set; }
+	/// <summary>
+	/// How many battery points each player receives every second
+	/// </summary>
+	public int RechargePointsPerSecond { get; set; }
+	/// <summary>
+	/// Password required to create a new game or begin game play
+	/// </summary>
+	public string? Password { get; set; }
+	/// <summary>
+	/// Which game instance you are starting/connecting to
+	/// </summary>
+	public required string GameID { get; set; }
 }
